97e251f5a2c9b62ceefede88261b13b4c8179139
Trevor <lingtrevor@gmail.com>
2019-04-29 15:39:23 +0300

    Added Sign In and Sign Up forms
    Sign In and Sign Up components are dynamically changeable

---
+++ client/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <UserForm></UserForm>
+  </div>
+</template>
+
+<script>
+import UserForm from "@/components/UserForm";
+export default {
+  name: "app",
+  components: {
+    UserForm
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+@import "./scss/_variables.scss";
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+
+    h1 {
+      color: $color-primary-dark;
+      padding: 10px 15px;
+    }
+  }
+}
+</style>
